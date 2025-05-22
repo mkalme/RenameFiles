5a8269f74b35acb21e2b53aa13fe4166d2b57482
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:16:45 +0300

    Add project files.

---
+++ RenameFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace RenameFiles
+{
+    class Program
+    {
+        private static string folderPath = @"D:\Backup\Pictures & Videos on Phone\Ceļojumi\Ceļojums-Itālija 2017\Bildes & Video\number2";
+
+        static void Main(string[] args)
+        {
+
+            renameAllFiles();
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static void renameAllFiles() {
+            string[] filePaths = Directory.GetFiles(folderPath);
+
+            for (int i = 0; i < filePaths.Length; i++) {
+                renameOneFile(filePaths[i], Path.GetDirectoryName(filePaths[i]) + @"\" + (i + 2000) + Path.GetExtension(filePaths[i]));
+            }
+        }
+
+        static void renameOneFile(string oldPath, string newPath) {
+            File.Move(oldPath, newPath);
+        }
+    }
+}
